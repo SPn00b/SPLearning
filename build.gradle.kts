f50f13b82d9e64f0cb46268ffcd948d36a2fba6d
SPn00b <0s71uxw4f@relay.firefox.com>
2025-06-16 07:37:01 +0530

    Added Apache Commons Libs for future usage.

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,33 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter")
     // https://mvnrepository.com/artifact/org.apache.logging.log4j/log4j-core
     implementation("org.apache.logging.log4j:log4j-core:2.24.3")
+
+    // https://mvnrepository.com/artifact/org.apache.commons/commons-lang3
+    implementation("org.apache.commons:commons-lang3:3.17.0")
+
+    // https://mvnrepository.com/artifact/org.apache.commons/commons-collections4
+    implementation("org.apache.commons:commons-collections4:4.5.0")
+
+    // https://mvnrepository.com/artifact/org.apache.commons/commons-compress
+    implementation("org.apache.commons:commons-compress:1.27.1")
+
+    // https://mvnrepository.com/artifact/org.apache.commons/commons-text
+    implementation("org.apache.commons:commons-text:1.13.1")
+
+    // https://mvnrepository.com/artifact/org.apache.commons/commons-math4-core
+    implementation("org.apache.commons:commons-math4-core:4.0-beta1")
+
+    // https://mvnrepository.com/artifact/org.apache.commons/commons-pool2
+    implementation("org.apache.commons:commons-pool2:2.12.1")
+
+    // https://mvnrepository.com/artifact/commons-io/commons-io
+    implementation("commons-io:commons-io:2.19.0")
+
+    // https://mvnrepository.com/artifact/org.apache.commons/commons-jexl3
+    implementation("org.apache.commons:commons-jexl3:3.5.0")
+
+    // https://mvnrepository.com/artifact/org.apache.commons/commons-exec
+    implementation("org.apache.commons:commons-exec:1.5.0")
 }
 
 tasks.test {
